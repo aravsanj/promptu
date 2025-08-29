f0d9c653a91f3dc3c1fb097671e3ca7bca6a5103
Aravind Sanjeev <saravind436@gmail.com>
2025-08-29 16:21:42 +0530

    fix add report an issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import GitHubIcon from "./icons/GitHub";
+
 const Header = () => (
   <header className="text-center mb-8">
     <h1 className="text-4xl font-bold text-gray-100 inline-flex items-center">
@@ -9,6 +11,16 @@ const Header = () => (
     <p className="text-lg mt-2 text-gray-400">
       Structured prompts with linting support (in-browser)
     </p>
+
+    <a
+      href="https://github.com/aravsanj/promptu/issues"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="mt-2 inline-flex items-center text-sm text-gray-500 hover:text-gray-300 transition-colors"
+    >
+      <GitHubIcon />
+      Report an issue
+    </a>
   </header>
 );
 
